91a2b2820ee4d2d5361df29c76177442ea11a02b
FernandoNovara <Fernando_Novara@hotmail.com>
2022-11-19 17:50:57 -0300

    Create
    Se creo el create y se puede ver los datos desde la base

--- controllers/PropietarioControllers.js
+++ controllers/PropietarioControllers.js
@@ -1,19 +1,38 @@
-const { render } = require("pug")
 const {dbConfig} = require("../database/db_con")
 
 module.exports = {
     
     async show(req,res){
 
-        const propietariosList = await dbConfig.Propietario.findAll()
+        try {
+            const propietariosList = await dbConfig.Propietario.findAll()
 
-        if(propietariosList.length > 0)
-        {
-            render(propietariosList)
+            if(propietariosList.length > 0)
+            {
+                res.render("Propietario",{allPropietario: propietariosList})
+            }
+            else
+            {
+                res.json({ msg: "No hay datos."})
+            }
+        } catch (error) {
+            console.log(error)
+            res.redirect("/")//falta mandar mensaje de error y redireccionar
         }
-        else
-        {
-            res.json({ msg: "No hay datos."})
+    },
+
+    async create(req,res)
+    {
+        try {
+            
+            const createPropietario = await dbConfig.Propietario.create(
+                {
+                    
+                }
+            )
+
+        } catch (error) {
+            console.log(error)
         }
     }
 }
